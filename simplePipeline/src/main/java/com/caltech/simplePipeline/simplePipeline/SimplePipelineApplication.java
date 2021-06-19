5e06d3cbf16c842d1faf80d648d67ffda6c7f797
student <diptisoftware@gmail.com>
2021-06-19 17:05:19 +0000

    commit

---
+++ simplePipeline/src/main/java/com/caltech/simplePipeline/simplePipeline/SimplePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.simplePipeline.simplePipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePipelineApplication.class, args);
+	}
+
+}
---
+++ simplePipeline/src/test/java/com/caltech/simplePipeline/simplePipeline/SimplePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.simplePipeline.simplePipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
